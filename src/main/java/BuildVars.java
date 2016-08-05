009570c4afcbf2321ffc6c7df574c73e42b6c575
Evgeniy Gusar <igusar@intropro.com>
2016-08-05 16:44:00 +0300

    Simple bot with /start hello command

---
+++ src/main/java/BuildVars.java
@@ -0,0 +1,7 @@
+/**
+ * Created by igusar on 8/5/16.
+ */
+public class BuildVars {
+    public static final String USERNAME = "HockeyAppCoolBot";
+    public static final String TOKEN = "220619973:AAHnyOr-PSPYYwCE-lB2KTAuSGNp8jtn9HQ";
+}
---
+++ src/main/java/HockeyAppBot.java
@@ -0,0 +1,56 @@
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+/**
+ * Created by igusar on 8/5/16.
+ */
+public class HockeyAppBot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new HockeyAppBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BuildVars.USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BuildVars.TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help"))
+                sendMsg(message, "Привет, я робот");
+            else
+                sendMsg(message, "Я не знаю что ответить на это");
+        }
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplayToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
